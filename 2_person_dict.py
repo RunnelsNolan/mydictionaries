36a866c56616b4391e3ee16d6c0b8237836c351a
RunnelsNolan <nolanrunnels@icloud.com>
2023-01-30 15:45:24 -0600

    modified

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -3,5 +3,20 @@ person["fname"] = "Joe"
 person["lname"] = "Fonebone"
 person["age"] = 51
 person["spouse"] = "Edna"
-person["children"] = ["Ralph", "Betty", "Joey"]
-person["pets"] = {"dog": "Fido", "cat": "Sox"}
+person["children"] = ["Ralph", "Betty", "Joey"] # value is a list
+person["pets"] = {"dog": "Fido", "cat": "Sox"} # value is a dictionary
+
+
+# print out name of the second child
+print(person["children"][1])
+
+# print out name of the cat
+print(person["pets"]['cat'])
+
+# iterate through all children and print out each child
+for child in person['children']:
+    print(child)
+
+# print out the pets in the format:
+
+# type of pet: dog name of pet: Fido
